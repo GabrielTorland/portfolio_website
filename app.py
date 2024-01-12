c77accb7d3bd2fecc612ae296328a45aaffa22ff
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2024-01-12 19:57:43 +0100

    Logger implemented

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from config import app, limiter
 from datetime import datetime, timedelta
 from collections import defaultdict
 from constants import DOMAIN_NAME, EMAIL_LIMIT
+from flask import jsonify
 
 @app.route('/')
 def index():
@@ -13,43 +14,74 @@ def index():
 @app.route('/send_email', methods=['POST'])
 @limiter.limit(f"{EMAIL_LIMIT} per day")
 def email_endpoint():
+    # Get the client's IP address
+    client_ip = request.remote_addr
+
+    # Log the IP address
+    app.logger.info(f"Email request received from IP: {client_ip}")
+
     fname = request.form.get('fname')
     lname = request.form.get('lname')
     email_address = request.form.get('email')
     subject = request.form.get('subject')
     message_content = request.form.get('message')
+
     store_email(db, fname, lname, email_address, subject, message_content)
+
     if not app.config['TESTING']:
         response = send_email(fname, lname, email_address, subject, message_content)
+        if "error" in response.json:
+            app.logger.error(response.json['error'])
+        else:
+            app.logger.info(response.json["message"])
     else:
-        response = {"message": "Simulated email sent successfully!"}
+        response = jsonify({"message": "Simulated email sent successfully!"})
+
     return response
 
 @app.route('/robots.txt')
 def static_from_root():
+    # Get the client's IP address
+    client_ip = request.remote_addr
+
+    # Log the IP address
+    app.logger.info(f"Email request received from IP: {client_ip}")
     return send_from_directory(app.static_folder, request.path[1:])
 
 @app.route('/sitemap.xml', methods=['GET'])
 def sitemap():
     """Generate sitemap.xml. Makes a list of urls/subfolder with date modified and priority."""
+
+    # Get the client's IP address
+    client_ip = request.remote_addr
+
+    # Log the IP address
+    app.logger.info(f"Email request received from IP: {client_ip}")
+
     pages = []
     ten_days_ago = datetime.now() - timedelta(days=10)
     page_priority = defaultdict(lambda: "0.2")
+
+    # Specify the priority of certain pages
     page_priority["/"] = "1.0"
+
+    # The application's URL map is iterated over to find all the routes that allow GET requests and have no arguments
     for rule in app.url_map.iter_rules():
         if "GET" in rule.methods and len(rule.arguments) == 0:
             pages.append(
                 [f"https://{DOMAIN_NAME}" + str(rule.rule), ten_days_ago, page_priority[str(rule.rule)]]
             )
-
+    # A sitemap is generated using a template and the list of pages
     sitemap_xml = render_template('sitemap_template.xml', pages=pages)
+
     response = make_response(sitemap_xml)
-    response.headers["Content-Type"] = "application/xml"    
+    response.headers["Content-Type"] = "application/xml"
 
     return response
 
 if __name__ == '__main__':
     if app.config["TMP_SQLITE_DATABASE"]:
+        app.logger.info("Creating temporary SQLite database.")
         with app.app_context():
             db.create_all()
     app.run(host="0.0.0.0", port=2387, debug=False)
--- config.py
+++ config.py
@@ -4,24 +4,59 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from werkzeug.middleware.proxy_fix import ProxyFix
 import os
+from logging.config import dictConfig
+
+# Configure logging
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] [%(levelname)s | %(module)s] %(message)s",
+                "datefmt": "%B %d, %Y %H:%M:%S %Z",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "default",
+            },
+            "file": {
+                "class": "logging.FileHandler",
+                "filename": "worldClock.log",
+                "formatter": "default",
+            },
+        },
+        "root": {"level": "DEBUG", "handlers": ["console", "file"]},
+    }
+)
 
 app = Flask(__name__)
 # Configure ProxyFix with the appropriate number of proxies
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+
+# Configure database
 database_uri = os.getenv('DATABASE_URI')
 if database_uri:
+    app.logger.info(f"Using provided database URI")
     app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
     app.config["TMP_SQLITE_DATABASE"] = False
 else:
+    app.logger.info(f"Using temporary SQLite database")
     app.config["TMP_SQLITE_DATABASE"] = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emails.db'
+
+# Do not track modifications to save memory
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-redis_url = os.getenv('REDIS_URL')
-
+# Setup database
 db = SQLAlchemy(app)
 
+# Configure rate limiting
+redis_url = os.getenv('REDIS_URL')
 if redis_url:
+    app.logger.info(f"Using provided Redis URL")
     limiter = Limiter(app=app, key_func=get_remote_address, storage_uri=redis_url, default_limits=["100 per day"])
 else:
+    app.logger.info(f"Using default rate limiting")
     limiter = Limiter(app=app, key_func=get_remote_address, default_limits=["100 per day"])
