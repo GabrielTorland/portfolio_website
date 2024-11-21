29962777bf508812db0ffc227fe15a6c444a3b84
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2024-11-21 22:24:18 +1000

    Added independent SMTP sender

--- email_utils.py
+++ email_utils.py
@@ -12,7 +12,7 @@ def store_email(db, fname, lname, email_address, subject, message_content):
 def send_email(fname, lname, email_address, subject, message_content):
     msg = MIMEText(f"dq8wmC&N89nEF8i^oRo$$Aq6bC\nFrom: {fname} {lname}\nEmail: {email_address}\n\n{message_content}")
     msg['Subject'] = subject
-    msg['From'] = os.getenv('SMTP_USER')
+    msg['From'] = os.getenv('SMTP_SENDER')
     msg['To'] = os.getenv('SMTP_RECEIVER')
 
     # SMTP server configuration and email sending logic
@@ -20,12 +20,12 @@ def send_email(fname, lname, email_address, subject, message_content):
         with SMTP(os.getenv('SMTP_SERVER'), int(os.getenv('SMTP_PORT'))) as server:
             server.starttls()
             server.login(os.getenv('SMTP_USER'), os.getenv('SMTP_PASSWORD'))
-            server.sendmail(os.getenv('SMTP_USER'), os.getenv('SMTP_RECEIVER'), msg.as_string())
+            server.sendmail(os.getenv('SMTP_SENDER'), os.getenv('SMTP_RECEIVER'), msg.as_string())
         return jsonify({"message": "Email sent successfully!"}), 200
     except SMTPConnectError:
         return jsonify({"message": "Failed to connect to the SMTP server."}), 500
     except SMTPAuthenticationError:
-        return jsonify({"message": "SMTP authentication failed. Check your username and password."}), 500 
+        return jsonify({"message": "SMTP authentication failed. Check your username and password."}), 500
     except SMTPRecipientsRefused:
         return jsonify({"message": "The recipient(s) was refused by the server."}), 500
     except SMTPSenderRefused:
